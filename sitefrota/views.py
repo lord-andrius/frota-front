f5e9655197da3997e6717d1e1083dc7976861cab
andrew dylan <andrewdylanvianaamorim.2@gmail.com>
2025-02-22 13:45:34 -0300

    pequenas mudancas

--- sitefrota/views.py
+++ sitefrota/views.py
@@ -10,6 +10,14 @@ def motorista(request):
     template = loader.get_template('motorista.html')
     return HttpResponse(template.render())
 
+def motoristaCriar(request):
+    template = loader.get_template('motoristacriar.html')
+    return HttpResponse(template.render())
+
+def motoristaEditar(request):
+    template = loader.get_template('motoristaeditar.html')
+    return HttpResponse(template.render())
+    
 def motivoDefeito(request):
     template = loader.get_template('motivodefeito.html')
     return HttpResponse(template.render())
@@ -22,8 +30,28 @@ def motivoDefeitoEditar(request):
     template = loader.get_template('motivodefeitoeditar.html')
     return HttpResponse(template.render())
 
+def tipoVeiculo(request):
+    template = loader.get_template('tipoveiculo.html')
+    return HttpResponse(template.render())
+
+def tipoVeiculoCriar(request):
+    template = loader.get_template('tipoveiculocriar.html')
+    return HttpResponse(template.render())
+
+def tipoVeiculoEditar(request):
+    template = loader.get_template('tipoveiculoeditar.html')
+    return HttpResponse(template.render())
+
 def veiculo(request):
     template = loader.get_template('veiculo.html')
+    return HttpResponse(template.render())
+
+def veiculoCriar(request):
+    template = loader.get_template('veiculocriar.html')
+    return HttpResponse(template.render())
+
+def veiculoEditar(request):
+    template = loader.get_template('veiculoeditar.html')
     return HttpResponse(template.render())
 
 def rota(request):
